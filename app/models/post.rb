c7e12ac05d9678aec693967ce60ef3ad0369e830
Drew Finelli <asfinelli@gmail.com>
2018-02-09 18:31:22 -0500

    heroku update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
 
-  has_attached_file :image, styles: { medium: "500x500>", large: "1000x1000", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "500x500>", large: "1000x1000", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
