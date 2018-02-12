5af1f6fc28d42d10e21d3ff86e7a00cf6618eb31
Drew Finelli <asfinelli@gmail.com>
2018-02-12 14:48:43 -0500

    updated navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :create]
+  before_action :authenticate_user!, only: [:show, :create, :new]
 
 
   def new
