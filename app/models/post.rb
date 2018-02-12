7189d0cdc8849c0ccd4d6a0106607c5ba63b199f
Drew Finelli <asfinelli@gmail.com>
2018-02-12 10:57:48 -0500

    added scrolling

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "500x500>", large: "1000x1000", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
+  def next_post
+    self.class.where("created_at > ?", created_at).order(created_at: :asc).first
+  end
+  def previous_post
+    self.class.where("created_at < ?", created_at).order(created_at: :desc).first
+  end
+
 end
