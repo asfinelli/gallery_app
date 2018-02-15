46a86865921602375da5c56d853711cda2b3f640
Drew Finelli <asfinelli@gmail.com>
2018-02-15 15:18:08 -0500

    styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-  @user = current_user
-  @post = Post.where(user_id: current_user.id)
-end
+    @user = current_user
+    @post = Post.where(user_id: current_user.id)
+  end
 end
